ee928bdd24971f3f1a0a460b6b108dc68e3ed3a0
Kush Thaker <kushthaker@gmail.com>
2015-06-11 13:43:24 -0400

    Cleaned up commented question formatting

--- intro-to-logic-problems.js
+++ intro-to-logic-problems.js
@@ -1,7 +1,8 @@
 
 // Chess board problem
 
-// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
+// At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 
 // Passing this string to console.log should show something like this:
 
@@ -14,7 +15,8 @@
 //  # # # #
 // # # # #
 
-// When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+// When you have a program that generates this pattern, define a variable size = 8 
+// and change the program so that it works for any size, outputting a grid of the given width and height.
 
 var size = 8;
 
@@ -39,7 +41,8 @@ console.log(str);
 
 
 // EXERCISE 
-// Create a function that takes in an integer and prints all numbers from 1 to the given integer the number of times equal to the current loop index (ie. 1, 2, 2, 3, 3, 3, ...).
+// Create a function that takes in an integer and prints all numbers from 1 to the given integer 
+// the number of times equal to the current loop index (ie. 1, 2, 2, 3, 3, 3, ...).
 // Alter your answer to exercise 2 to log the numbers in the form of a pyramid.
 
 var str = "";
